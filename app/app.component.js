21453746e2a06c028543073206ac51312f963427
lioff <lioff@qq.com>
2016-05-24 22:54:28 +0800

    hero list and edit is ok

--- app/app.component.js
+++ app/app.component.js
@@ -15,23 +15,34 @@ var Hero = (function () {
     return Hero;
 }());
 exports.Hero = Hero;
-//herotest: Hero = {
-//		  public id: 2,
-//		  public name: 'test will make erro'
-// }; 
-// console.log(herotest); // not defined
+//Hero array 
+var HEROES = [
+    { "id": 11, "name": "Mr.Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
-        this.hero = {
-            id: 1,
-            name: 'Windstorm'
-        };
+        //hero: Hero = {
+        //	  id: 11,
+        //	  name: 'Mr.Nice'
+        // };
+        this.heroes = HEROES;
     }
+    AppComponent.prototype.onSelect = function (hero) { this.selectedHero = hero; };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1> {{ title }}</h1> <p> hero: {{ hero.name}}  </p>'
+            template: "\n    <h1>{{title}}</h1>\n    <h2>My Heroes   | 1st: {{ HEROES }}</h2>\n    <ul class=\"heroes\">\n      <li *ngFor=\"let hero of heroes\"\n        [class.selected]=\"hero === selectedHero\"\n        (click)=\"onSelect(hero)\">\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>\n    <div *ngIf=\"selectedHero\">\n      <h2>{{selectedHero.name}} details!</h2>\n      <div><label>id: </label>{{selectedHero.id}}</div>\n      <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\"/>\n      </div>\n    </div>\n  ",
+            styles: ["\n\t  .selected {\n\t    background-color: #CFD8DC !important;\n\t    color: white;\n\t  }\n\t  .heroes {\n\t    margin: 0 0 2em 0;\n\t    list-style-type: none;\n\t    padding: 0;\n\t    width: 15em;\n\t  }\n\t  .heroes li {\n\t    cursor: pointer;\n\t    position: relative;\n\t    left: 0;\n\t    background-color: #EEE;\n\t    margin: .5em;\n\t    padding: .3em 0;\n\t    height: 1.6em;\n\t    border-radius: 4px;\n\t  }\n\t  .heroes li.selected:hover {\n\t    background-color: #BBD8DC !important;\n\t    color: white;\n\t  }\n\t  .heroes li:hover {\n\t    color: #607D8B;\n\t    background-color: #DDD;\n\t    left: .1em;\n\t  }\n\t  .heroes .text {\n\t    position: relative;\n\t    top: -3px;\n\t  }\n\t  .heroes .badge {\n\t    display: inline-block;\n\t    font-size: small;\n\t    color: white;\n\t    padding: 0.8em 0.7em 0 0.7em;\n\t    background-color: #607D8B;\n\t    line-height: 1em;\n\t    position: relative;\n\t    left: -1px;\n\t    top: -4px;\n\t    height: 1.8em;\n\t    margin-right: .8em;\n\t    border-radius: 4px 0 0 4px;\n\t  }\n\t"]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,26 +1,112 @@
 import { Component } from '@angular/core';
 
 export class Hero {
-  id: number;
-   name: string;
+  public id: number;
+  public name: string;
 }
 
-//herotest: Hero = {
-//		  public id: 2,
-//		  public name: 'test will make erro'
-// }; 
-// console.log(herotest); // not defined
+
+//Hero array 
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr.Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+]
+ 
+ 
   
 @Component({
   selector: 'my-app',
-  template: '<h1> {{ title }}</h1> <p> hero: {{ hero.name}}  </p>'
+  template:`
+    <h1>{{title}}</h1>
+    <h2>My Heroes   | 1st: {{ HEROES }}</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
+    </div>
+  `,
+  styles:[`
+	  .selected {
+	    background-color: #CFD8DC !important;
+	    color: white;
+	  }
+	  .heroes {
+	    margin: 0 0 2em 0;
+	    list-style-type: none;
+	    padding: 0;
+	    width: 15em;
+	  }
+	  .heroes li {
+	    cursor: pointer;
+	    position: relative;
+	    left: 0;
+	    background-color: #EEE;
+	    margin: .5em;
+	    padding: .3em 0;
+	    height: 1.6em;
+	    border-radius: 4px;
+	  }
+	  .heroes li.selected:hover {
+	    background-color: #BBD8DC !important;
+	    color: white;
+	  }
+	  .heroes li:hover {
+	    color: #607D8B;
+	    background-color: #DDD;
+	    left: .1em;
+	  }
+	  .heroes .text {
+	    position: relative;
+	    top: -3px;
+	  }
+	  .heroes .badge {
+	    display: inline-block;
+	    font-size: small;
+	    color: white;
+	    padding: 0.8em 0.7em 0 0.7em;
+	    background-color: #607D8B;
+	    line-height: 1em;
+	    position: relative;
+	    left: -1px;
+	    top: -4px;
+	    height: 1.8em;
+	    margin-right: .8em;
+	    border-radius: 4px 0 0 4px;
+	  }
+	`]
 })
+
+//
+
+ 
+ 
 export class AppComponent { 
 	  title = 'Tour of Heroes';
-      hero: Hero = {
-		  public id: 1,
-		  public name: 'Windstorm'
-	  };
+      //hero: Hero = {
+	  //	  id: 11,
+	  //	  name: 'Mr.Nice'
+	  // };
+	  heroes = HEROES;
+	  selectedHero ;//= HEROES[0]; // 设置默认选中 未实现
+	  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
 
 
